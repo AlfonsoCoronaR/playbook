393b14ee9a7827a2154cebc400dd94eff042e868
AlfonsoCoronaR <acr.redb@gmail.com>
2022-04-11 17:25:48 -0500

    Ejercicios semana 1

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log(`Mi pokemon ${this.name} te saluda`);
+  }
+
+  sayMessage(palabra) {
+    console.log(`Mi pokemon ${this.name} dice: ${palabra}`);
+  }
+}
+
+module.exports = Pokemon;
